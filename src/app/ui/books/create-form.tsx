970ebe411458480f2fa8b48b721834279afc541d
Santiago GRJ <santiago-2003-rojas@hotmail.com>
2025-01-27 11:08:32 -0600

    Center text Button Cancel

--- src/app/ui/books/create-form.tsx
+++ src/app/ui/books/create-form.tsx
@@ -132,7 +132,7 @@ export default function CreateForm() {
         <div className="mt-6 flex justify-end gap-4">
           <Link
             href={"/books"}
-            className="rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+            className="flex items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
           >
             Cancel
           </Link>
