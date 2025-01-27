a37f63c0c94424f4724df6f6de7fb38cc8ac106e
Santiago GRJ <santiago-2003-rojas@hotmail.com>
2025-01-27 14:07:55 -0600

    Changes

--- src/app/books/layout.tsx
+++ src/app/books/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout(
         <div className="w-full flex-none md:w-64">
             <SideNav/>
         </div>
-         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+         <div className="flex-grow p-4 md:overflow-x-auto md:p-12">{children}</div>
        </div>
     )
 }
--- src/app/ui/books/table.tsx
+++ src/app/ui/books/table.tsx
@@ -1,13 +1,17 @@
 import { fetchBooks, fetchFilteredBooks } from "@/lib/data";
 import { DeleteBook, UpdateBook } from "./buttons";
 
-
-export default async function BooksTable({query,currentPage}:{query:string,currentPage:number}) {
-  const data = await fetchFilteredBooks(query,currentPage);
-  
+export default async function BooksTable({
+  query,
+  currentPage,
+}: {
+  query: string;
+  currentPage: number;
+}) {
+  const data = await fetchFilteredBooks(query, currentPage);
 
   return (
-    <table className=" min-w-full text-gray-900 md:table ">
+    <table className="  text-gray-900 ">
       <thead className="rounded-lg text-left text-sm font-normal">
         <tr className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
           {/* <th scope="col" className="px-4 py-5">
@@ -35,14 +39,14 @@ export default async function BooksTable({query,currentPage}:{query:string,curre
           data.map((book) => (
             <tr className="" key={book.id}>
               {/* <td className="whitespace-nowrap px-3 py-3">{book.id}</td> */}
-              <td className="whitespace-nowrap px-3 py-3">{book.title}</td>
-              <td className="whitespace-nowrap px-3 py-3">{book.description}</td>
-              <td className="whitespace-nowrap px-3 py-3">{book.author}</td>
+              <td className="px-2 py-3">{book.title}</td>
+              <td className=" px-2 py-3">{book.description}</td>
+              <td className=" px-2 py-3">{book.author}</td>
               <td className="whitespace-nowrap px-3 py-3">{book.price}</td>
               <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                <div className="flex justify-start gap-3">
-                    <UpdateBook id={book.id} />
-                    <DeleteBook id={book.id} />
+                <div className="flex flex-wrap justify-start gap-3">
+                  <UpdateBook id={book.id} />
+                  <DeleteBook id={book.id} />
                 </div>
               </td>
             </tr>
